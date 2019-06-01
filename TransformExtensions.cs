4524b1bc92a0fb0997ff250ece0751ae8f7e9b16
Daniel Schulz <faulolio@gmail.com>
2019-06-01 16:04:54 +0200

    Transform

---
+++ TransformExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Slothsoft.UnityExtensions {
+    public static class TransformExtensions {
+        public static void SetX(this Transform transform, float value) {
+            transform.position = new Vector3(value, transform.position.y, transform.position.z);
+        }
+        public static void SetY(this Transform transform, float value) {
+            transform.position = new Vector3(transform.position.x, value, transform.position.z);
+        }
+        public static void SetZ(this Transform transform, float value) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, value);
+        }
+    }
+}
