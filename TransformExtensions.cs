bf919d991139d41e201045711f3303a49a350694
Daniel Schulz <faulolio@gmail.com>
2019-06-15 14:38:37 +0200

    SetScale

--- TransformExtensions.cs
+++ TransformExtensions.cs
@@ -13,5 +13,14 @@ namespace Slothsoft.UnityExtensions {
         public static void SetZ(this Transform transform, float value) {
             transform.position = new Vector3(transform.position.x, transform.position.y, value);
         }
+        public static void SetScaleX(this Transform transform, float value) {
+            transform.localScale = new Vector3(value, transform.localScale.y, transform.localScale.z);
+        }
+        public static void SetScaleY(this Transform transform, float value) {
+            transform.localScale = new Vector3(transform.localScale.x, value, transform.localScale.z);
+        }
+        public static void SetScaleZ(this Transform transform, float value) {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, value);
+        }
     }
 }
