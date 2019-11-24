385aae2be340374773d2e685d7e01e81ed1cba15
Daniel Schulz <faulolio@gmail.com>
2019-11-24 16:40:27 +0100

    add tests for Transform

---
+++ Packages/Slothsoft.UnityExtensions/Tests/Runtime/TransformTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Slothsoft.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class TransformTest {
+    [Test]
+    public void TestSetX() {
+        var gameObject = new GameObject();
+        gameObject.transform.position = Vector3.zero;
+
+        gameObject.transform.SetX(10);
+
+        Assert.AreEqual(gameObject.transform.position, new Vector3(10, 0, 0));
+    }
+    [Test]
+    public void TestSetY() {
+        var gameObject = new GameObject();
+        gameObject.transform.position = Vector3.zero;
+
+        gameObject.transform.SetY(10);
+
+        Assert.AreEqual(gameObject.transform.position, new Vector3(0, 10, 0));
+    }
+    [Test]
+    public void TestSetZ() {
+        var gameObject = new GameObject();
+        gameObject.transform.position = Vector3.zero;
+
+        gameObject.transform.SetZ(10);
+
+        Assert.AreEqual(gameObject.transform.position, new Vector3(0, 0, 10));
+    }
+
+    [Test]
+    public void TestSetScaleX() {
+        var gameObject = new GameObject();
+        gameObject.transform.localScale = Vector3.one;
+
+        gameObject.transform.SetScaleX(10);
+
+        Assert.AreEqual(gameObject.transform.localScale, new Vector3(10, 1, 1));
+    }
+    [Test]
+    public void TestSetScaleY() {
+        var gameObject = new GameObject();
+        gameObject.transform.localScale = Vector3.one;
+
+        gameObject.transform.SetScaleY(10);
+
+        Assert.AreEqual(gameObject.transform.localScale, new Vector3(1, 10, 1));
+    }
+    [Test]
+    public void TestSetScaleZ() {
+        var gameObject = new GameObject();
+        gameObject.transform.localScale = Vector3.one;
+
+        gameObject.transform.SetScaleZ(10);
+
+        Assert.AreEqual(gameObject.transform.localScale, new Vector3(1, 1, 10));
+    }
+}
