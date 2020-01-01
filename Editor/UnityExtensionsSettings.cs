b0f9551435b427eed08e3321ca8a8f9cd3cf2047
Daniel Schulz <faulolio@gmail.com>
2020-01-01 18:26:55 +0100

    Add caching to Settings instance

--- Editor/UnityExtensionsSettings.cs
+++ Editor/UnityExtensionsSettings.cs
@@ -7,7 +7,16 @@ using UnityEngine;
 namespace Slothsoft.UnityExtensions.Editor {
     [CreateAssetMenu(menuName = "Slothsoft/Unity Extension Settings", fileName = "Slothsoft.UnityExtensionsSettings.asset")]
     internal class UnityExtensionsSettings : ScriptableObject {
-        internal static UnityExtensionsSettings instance => Resources.LoadAll<UnityExtensionsSettings>("").FirstOrDefault();
+        internal static UnityExtensionsSettings instance {
+            get {
+                if (instanceCache == null) {
+                    instanceCache = Resources.LoadAll<UnityExtensionsSettings>("").FirstOrDefault();
+                }
+                return instanceCache;
+            }
+        }
+        internal static UnityExtensionsSettings instanceCache;
+
         [SerializeField, Tooltip("Use the following options to change the style of the [Expandable] ScriptableObject drawers")]
         public ExpandableSettings expandableSettings = new ExpandableSettings();
     }
