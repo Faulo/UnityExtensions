e25a966823a67c3b50919bde3b62a14c6d0c4e73
Daniel Schulz <faulolio@gmail.com>
2020-06-29 23:15:27 +0200

    Add more asset loading methods

--- Editor/PrefabUtils.cs
+++ Editor/PrefabUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -12,5 +13,20 @@ namespace Slothsoft.UnityExtensions.Editor {
             .Where(path => path.EndsWith(settings.prefabExtension))
             .Select(AssetDatabase.LoadMainAssetAtPath)
             .OfType<GameObject>();
+
+        public static T LoadAssetAtPath<T>(FileSystemInfo file) where T : Object {
+            string path = file.FullName;
+            string root = new DirectoryInfo(".").FullName;
+            if (path.StartsWith(root)) {
+                path = path.Substring(root.Length + 1);
+            }
+            return AssetDatabase.LoadAssetAtPath<T>(path);
+        }
+
+        public static IEnumerable<T> LoadAssets<T>(string searchFolder = "Assets") where T : Object {
+            return AssetDatabase.FindAssets($"t:{typeof(T).Name}", new[] { searchFolder })
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<T>);
+        }
     }
 }
