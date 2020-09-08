0d7d69c5a3db2a398d5a183f2b2b11bb1d58c7f3
Daniel Schulz <faulolio@gmail.com>
2020-09-08 17:33:26 +0200

    Implement [Layer] attribute

---
+++ Runtime/LayerAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Slothsoft.UnityExtensions {
+    /// <summary>
+    /// Attribute to select a single layer.
+    /// </summary>
+    public class LayerAttribute : PropertyAttribute {
+    }
+}
