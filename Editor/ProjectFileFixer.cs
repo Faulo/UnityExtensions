e20844680bcd6a4aa9052f4e89b04c951ae78992
Daniel Schulz <faulolio@gmail.com>
2020-11-06 23:14:40 +0100

    Make ProjectFileFixer compatible with Unity 2020.1+

--- Editor/ProjectFileFixer.cs
+++ Editor/ProjectFileFixer.cs
@@ -5,20 +5,15 @@ using System.Xml.Linq;
 using UnityEditor;
 
 namespace Slothsoft.UnityExtensions.Editor {
-    [InitializeOnLoad]
-    public class ProjectFileFixer {
+    public class ProjectFileFixer : AssetPostprocessor {
         const string PROJECT_FILE_EXTENSION = ".csproj";
         const string NS_CSPROJ = "http://schemas.microsoft.com/developer/msbuild/2003";
 
         class Utf8StringWriter : StringWriter {
             public override Encoding Encoding => Encoding.UTF8;
         }
-        static ProjectFileFixer() {
-#if ENABLE_VSTU
-            SyntaxTree.VisualStudio.Unity.Bridge.ProjectFilesGenerator.ProjectFileGeneration += ProjectFileGenerationListener;
-#endif
-        }
-        static string ProjectFileGenerationListener(string fileName, string fileContent) {
+
+        public static string OnGeneratedCSProject(string fileName, string fileContent) {
             if (UnityExtensionsSettings.instance.projectFileSettingsEnabled) {
                 var file = new FileInfo(fileName);
                 if (file.Extension == PROJECT_FILE_EXTENSION) {
