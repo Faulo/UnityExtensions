168b73dc4c01d8d897e9a2dbd95adba6657d5b36
Daniel Schulz <faulolio@gmail.com>
2021-02-28 16:21:28 +0100

    Restructure tests

---
+++ Tests/EditMode/ExpandableAttributeTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Slothsoft.UnityExtensions.Tests {
+    public class ExpandableAttributeTests {
+        interface IOne {
+        }
+        interface ITwo {
+        }
+        class A : MonoBehaviour, IOne, ITwo {
+        }
+        class B : MonoBehaviour {
+        }
+        [Test]
+        public void TestClassImplementsInterfaces() {
+            var attribute = new ExpandableAttribute(typeof(IOne));
+            var obj = new GameObject().AddComponent<A>();
+
+            Assert.IsTrue(attribute.ValidateType(obj), $"{typeof(A)} must pass validation.");
+        }
+        [Test]
+        public void TestClassDoesNotImplementInterfaces() {
+            var attribute = new ExpandableAttribute(typeof(ITwo));
+            var obj = new GameObject().AddComponent<B>();
+
+            Assert.IsFalse(attribute.ValidateType(obj), $"{typeof(B)} must not pass validation.");
+        }
+    }
+}
