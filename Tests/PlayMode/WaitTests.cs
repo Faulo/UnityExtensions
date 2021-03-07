29c2443ed8bb8ec6f237142acaacfecab05c0281
Daniel Schulz <faulolio@gmail.com>
2021-03-07 14:07:43 +0100

    Introduce Wait

---
+++ Tests/PlayMode/WaitTests.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Diagnostics;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Slothsoft.UnityExtensions.Tests {
+    public class WaitTests {
+        [UnityTest]
+        public IEnumerator TestWaitForEndOfFrame() {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            yield return Wait.forEndOfFrame;
+            stopWatch.Stop();
+            Assert.Less(stopWatch.Elapsed.TotalSeconds, Time.fixedDeltaTime);
+        }
+        [UnityTest]
+        public IEnumerator TestWaitFixedUpdate() {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            yield return Wait.forFixedUpdate;
+            stopWatch.Stop();
+            Assert.Less(stopWatch.Elapsed.TotalSeconds, Time.fixedDeltaTime);
+        }
+        [UnityTest]
+        public IEnumerator TestWaitForSeconds() {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            yield return Wait.forSeconds[0.1f];
+            stopWatch.Stop();
+            Assert.Greater(stopWatch.Elapsed.TotalSeconds, 0.1f);
+        }
+        [UnityTest]
+        public IEnumerator TestWaitForSecondsRealtime() {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            yield return Wait.forSecondsRealtime[0.1f];
+            stopWatch.Stop();
+            Assert.Greater(stopWatch.Elapsed.TotalSeconds, 0.1f);
+        }
+    }
+}
