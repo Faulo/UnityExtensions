8e1e99bebff53e2f9bcc0ceecbeba30b3216aa91
Daniel Schulz <faulolio@gmail.com>
2022-03-04 17:02:02 +0100

    Add transform tests

--- Tests/PlayMode/WaitTests.cs
+++ Tests/PlayMode/WaitTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Slothsoft.UnityExtensions.Tests {
+namespace Slothsoft.UnityExtensions.Tests.PlayMode {
     public class WaitTests {
         [UnityTest]
         public IEnumerator TestWaitForEndOfFrame() {
