099933328a02eb2ec896a060d6253c172b7a16e7
Daniel Schulz <faulolio@gmail.com>
2022-03-04 18:02:38 +0100

    Add transform tests

--- Tests/EditMode/TransformTests.cs
+++ Tests/EditMode/TransformTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using UnityEngine;
 
 namespace Slothsoft.UnityExtensions.Tests.EditMode {
@@ -33,6 +34,104 @@ namespace Slothsoft.UnityExtensions.Tests.EditMode {
             context.transform.Clear();
 
             Assert.AreEqual(0, context.transform.childCount);
+        }
+
+        [Test]
+        public void TestTryGetComponentInChildrenReturnsSelf() {
+            var context = CreateHierarchy();
+
+            bool success = context.TryGetComponentInChildren<Transform>(out var result);
+
+            Assert.IsTrue(success);
+            Assert.AreEqual(context, result);
+        }
+
+        [Test]
+        public void TestTryGetComponentInParentReturnsSelf() {
+            var context = CreateHierarchy();
+
+            bool success = context.TryGetComponentInParent<Transform>(out var result);
+
+            Assert.IsTrue(success);
+            Assert.AreEqual(context, result);
+        }
+
+        [Test]
+        public void TestTryGetComponentInHierarchyReturnsSelf() {
+            var context = CreateHierarchy();
+
+            bool success = context.TryGetComponentInHierarchy<Transform>(out var result);
+
+            Assert.IsTrue(success);
+            Assert.AreEqual(context, result);
+        }
+
+        [TestCase(nameof(Transform), true)]
+        [TestCase(nameof(Renderer), false)]
+        [TestCase(nameof(BoxCollider), false)]
+        [TestCase(nameof(SphereCollider), true)]
+        public void TestTryGetComponentInChildren(string type, bool expectedResult) {
+            var context = CreateHierarchy();
+
+            bool actualresult = type switch {
+                nameof(Transform) => context.TryGetComponentInChildren<Transform>(out _),
+                nameof(Renderer) => context.TryGetComponentInChildren<Renderer>(out _),
+                nameof(BoxCollider) => context.TryGetComponentInChildren<BoxCollider>(out _),
+                nameof(SphereCollider) => context.TryGetComponentInChildren<SphereCollider>(out _),
+                _ => throw new NotImplementedException(),
+            };
+
+            Assert.AreEqual(expectedResult, actualresult);
+        }
+
+        [TestCase(nameof(Transform), true)]
+        [TestCase(nameof(Renderer), false)]
+        [TestCase(nameof(BoxCollider), true)]
+        [TestCase(nameof(SphereCollider), false)]
+        public void TestTryGetComponentInParent(string type, bool expectedResult) {
+            var context = CreateHierarchy();
+
+            bool actualresult = type switch {
+                nameof(Transform) => context.TryGetComponentInParent<Transform>(out _),
+                nameof(Renderer) => context.TryGetComponentInParent<Renderer>(out _),
+                nameof(BoxCollider) => context.TryGetComponentInParent<BoxCollider>(out _),
+                nameof(SphereCollider) => context.TryGetComponentInParent<SphereCollider>(out _),
+                _ => throw new NotImplementedException(),
+            };
+
+            Assert.AreEqual(expectedResult, actualresult);
+        }
+
+        [TestCase(nameof(Transform), true)]
+        [TestCase(nameof(Renderer), false)]
+        [TestCase(nameof(BoxCollider), true)]
+        [TestCase(nameof(SphereCollider), true)]
+        public void TestTryGetComponentInHierarchy(string type, bool expectedResult) {
+            var context = CreateHierarchy();
+
+            bool actualresult = type switch {
+                nameof(Transform) => context.TryGetComponentInHierarchy<Transform>(out _),
+                nameof(Renderer) => context.TryGetComponentInHierarchy<Renderer>(out _),
+                nameof(BoxCollider) => context.TryGetComponentInHierarchy<BoxCollider>(out _),
+                nameof(SphereCollider) => context.TryGetComponentInHierarchy<SphereCollider>(out _),
+                _ => throw new NotImplementedException(),
+            };
+
+            Assert.AreEqual(expectedResult, actualresult);
+        }
+
+        Transform CreateHierarchy() {
+            var parent = new GameObject();
+            var context = new GameObject();
+            var child = new GameObject();
+
+            context.transform.parent = parent.transform;
+            child.transform.parent = context.transform;
+
+            parent.AddComponent<BoxCollider>();
+            child.AddComponent<SphereCollider>();
+
+            return context.transform;
         }
     }
 }
