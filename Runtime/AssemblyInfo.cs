26a5e467a1607af950cbf424f8d8e4b8fce85716
Daniel Schulz <faulolio@gmail.com>
2022-10-04 12:23:15 +0200

    Add AssemblyInfos

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,3 +1,15 @@
 using System.Runtime.CompilerServices;
+using Slothsoft.UnityExtensions;
 
-[assembly: InternalsVisibleTo("Slothsoft.UnityExtensions.Runtime.Tests")]
+[assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_EDITOR)]
+[assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_TESTS_PLAYMODE)]
+[assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_TESTS_EDITMODE)]
+
+namespace Slothsoft.UnityExtensions {
+    static class AssemblyInfo {
+        public const string NAMESPACE_RUNTIME = "Slothsoft.UnityExtensions";
+        public const string NAMESPACE_EDITOR = "Slothsoft.UnityExtensions.Editor";
+        public const string NAMESPACE_TESTS_PLAYMODE = "Slothsoft.UnityExtensions.Tests.PlayMode";
+        public const string NAMESPACE_TESTS_EDITMODE = "Slothsoft.UnityExtensions.Tests.EditMode";
+    }
+}
---
+++ Tests/EditMode/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using NUnit.Framework;
+using Slothsoft.UnityExtensions;
+using UnityEngine.TestTools;
+
+[assembly: TestMustExpectAllLogs]
+[assembly: Category(AssemblyInfo.NAMESPACE_RUNTIME)]
