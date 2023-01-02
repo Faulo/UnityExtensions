ec617efd7c6232bc106193b887d72a11831bd0c9
Daniel Schulz <faulolio@gmail.com>
2023-01-02 12:20:00 +0100

    Use CSharpUtils

---
+++ Editor/CSharpFileFixer/CSharpModificationProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Slothsoft.UnityExtensions.Editor.PackageSettings;
+using UnityEditor;
+
+namespace Slothsoft.UnityExtensions.Editor.CSharpFileFixer {
+    sealed class CSharpModificationProcessor : UnityEditor.AssetModificationProcessor {
+        static void OnWillCreateAsset(string path) {
+            if (!UnityExtensionsSettings.instance.cSharpSettings.addNamespaceToCSharpFiles) {
+                return;
+            }
+
+            path = AssetDatabase.GetAssetPathFromTextMetaFilePath(path);
+
+            CSharpUtils.TryReplacePlaceholders(path);
+        }
+    }
+}
