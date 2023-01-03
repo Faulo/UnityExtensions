7c131b743c5d46c7795715cbe3159e1792271d48
Daniel Schulz <faulolio@gmail.com>
2023-01-03 11:50:04 +0100

    Fix tooltip

--- Editor/PackageSettings/PrefabUtilsSettings.cs
+++ Editor/PackageSettings/PrefabUtilsSettings.cs
@@ -5,8 +5,9 @@ namespace Slothsoft.UnityExtensions.Editor.PackageSettings {
     [Serializable]
     sealed class PrefabUtilsSettings {
         [Header("File system configuration")]
-        [SerializeField, Tooltip("Root path to load prefabs from, if load.")]
+        [SerializeField, Tooltip("Root path to load prefabs from when using PrefabUtils.allPrefabs.")]
         internal string assetsFolder = "Assets/";
+
         [SerializeField, Tooltip("File extension of prefabs to load, if any.")]
         internal string prefabExtension = ".prefab";
     }
