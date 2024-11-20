80e0da53a57d9e74c37f9474bd2eadb52995e66f
Daniel Schulz <faulolio@gmail.com>
2024-11-20 22:23:31 +0100

    docs: add summaries

--- Runtime/BoundsIntExtensions.cs
+++ Runtime/BoundsIntExtensions.cs
@@ -1,11 +1,27 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace Slothsoft.UnityExtensions {
     public static class BoundsIntExtensions {
+        /// <summary>
+        /// Same as <see cref="Bounds.Contains(Vector3)"/>, but only compare x and y of <paramref name="position"/>.
+        /// </summary>
+        /// <param name="bounds"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Contains2D(this in BoundsInt bounds, in Vector2Int position) {
             return position.x >= bounds.xMin && position.x < bounds.xMax
                 && position.y >= bounds.yMin && position.y < bounds.yMax;
         }
+
+        /// <summary>
+        /// Same as <see cref="Bounds.Contains(Vector3)"/>, but only compare x and y of <paramref name="position"/>.
+        /// </summary>
+        /// <param name="bounds"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Contains2D(this in BoundsInt bounds, in Vector3Int position) {
             return position.x >= bounds.xMin && position.x < bounds.xMax
                 && position.y >= bounds.yMin && position.y < bounds.yMax;
--- Runtime/ColorExtensions.cs
+++ Runtime/ColorExtensions.cs
@@ -7,15 +7,44 @@ namespace Slothsoft.UnityExtensions {
             green = color.g;
             blue = color.b;
         }
+
         public static void Deconstruct(this Color color, out float red, out float green, out float blue, out float alpha) {
             red = color.r;
             green = color.g;
             blue = color.b;
             alpha = color.a;
         }
+
+        /// <summary>
+        /// Create a copy of <paramref name="color"/>, replacing <paramref name="red"/>.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="red"></param>
+        /// <returns></returns>
         public static Color WithRed(this Color color, float red) => new(red, color.g, color.b, color.a);
+
+        /// <summary>
+        /// Create a copy of <paramref name="color"/>, replacing <paramref name="green"/>.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="red"></param>
+        /// <returns></returns>
         public static Color WithGreen(this Color color, float green) => new(color.r, green, color.b, color.a);
+
+        /// <summary>
+        /// Create a copy of <paramref name="color"/>, replacing <paramref name="blue"/>.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="red"></param>
+        /// <returns></returns>
         public static Color WithBlue(this Color color, float blue) => new(color.r, color.g, blue, color.a);
+
+        /// <summary>
+        /// Create a copy of <paramref name="color"/>, replacing <paramref name="alpha"/>.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="red"></param>
+        /// <returns></returns>
         public static Color WithAlpha(this Color color, float alpha) => new(color.r, color.g, color.b, alpha);
     }
 }
